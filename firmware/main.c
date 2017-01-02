7c21f4bbdee294e446d336c55d234b916f8891aa
Nathan Witmer <nathan@zerowidth.com>
2017-01-01 21:01:18 -0700

    Write to neopixel instead of LED pin

--- firmware/main.c
+++ firmware/main.c
@@ -5,11 +5,54 @@
 #include <util/delay.h>
 #include "softuart.h"
 
+/* how many pixels are available to write? */
+#define PIXELS 16
+
 #define PIXEL_PORT PORTA
 #define PIXEL_DDR  DDRA
 #define PIXEL_BIT  PORTA5
 /* PA5 is pin 8 */
 /* softuart uses PA1 for RX, PA2 for TX (pins 12 and 11) */
+
+#define SCALE(val) ((val) >> 1)
+
+static uint8_t grb[PIXELS*3];
+
+void set_pixel(uint8_t i, uint8_t r, uint8_t g, uint8_t b)
+{
+  uint8_t offset = (i % PIXELS) * 3;
+  grb[offset] = SCALE(g);
+  grb[offset+1] = SCALE(r);
+  grb[offset+2] = SCALE(b);
+}
+
+void write_pixels() {
+  cli(); /* disable interrupts */
+  asm volatile(
+      "1:"                    "\n\t" /* outer loop: iterate bytes */
+      "ld %[byte], %a[grb]+"  "\n\t"
+      "ldi %[bits], 8"        "\n\t"
+      "2:"                    "\n\t" /* inner loop: write a byte */
+      "sbi %[port], %[pin]"   "\n\t" /* t = 0 */
+      "sbrs %[byte], 7"       "\n\t" /* 2c if skip, 1c if no skip */
+      "cbi %[port], %[pin]"   "\n\t" /* 2c, t1 = 375ns */
+      "lsl %[byte]"           "\n\t" /* 1c, t = 500 (0) / 375 (1) */
+      "dec %[bits]"           "\n\t" /* 1c, t = 625 (0) / 500 (1) */
+      "nop"                   "\n\t" /* 1c, t = 750 (0) / 625 (1) */
+      "cbi %[port], %[pin]"   "\n\t" /* 2c, t2= 875 (0) / 750 (1) */
+      "brne 2b"               "\n\t" /* 2c if skip, 1c if not */
+      "dec %[nbytes]"         "\n\t"
+      "brne 1b"               "\n\t"
+      :: [nbytes]  "d" (PIXELS * 3)  /* how many pixels to write */
+      ,  [grb]     "w" (grb)         /* pointer to grb byte array */
+      ,  [byte]    "d" (16)
+      ,  [bits]    "d" (17)
+      ,  [port]    "i" (_SFR_IO_ADDR(PIXEL_PORT))
+      ,  [pin]     "i" (PIXEL_BIT)
+      );
+  _delay_us(10);
+  sei(); /* enable interrupts */
+}
 
 void check_serial()
 {
@@ -17,14 +60,13 @@ void check_serial()
   if (softuart_kbhit()) {
     c = softuart_getchar();
     if (c & 0x1) {
-      PIXEL_PORT |= _BV(PIXEL_BIT);
+      set_pixel(0, 255, 0, 0);
     }
     else {
-      PIXEL_PORT &= ~_BV(PIXEL_BIT);
+      set_pixel(0, 0, 0, 255);
     }
+    write_pixels();
     softuart_putchar(c);
-    softuart_putchar('\r');
-    softuart_putchar('\n');
   }
 }
 
