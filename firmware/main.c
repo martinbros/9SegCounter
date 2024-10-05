b79615d942b96e629b36d53b86446f417b303367
martinbros <67989270+martinbros@users.noreply.github.com>
2024-10-05 10:49:56 -0700

    Making edits to support SAO.

--- firmware/main.c
+++ firmware/main.c
@@ -1,17 +1,15 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdlib.h>
-#include <string.h>
-#include <avr/pgmspace.h> /* for puts_P macro support */
 #include <util/delay.h>
-#include "softuart.h"
+
+#define F_CPU 8000000UL
 
 /* how many pixels are available to write? */
-#define PIXELS 16
+#define PIXELS 18
 
-#define PIXEL_PORT PORTA
-#define PIXEL_DDR  DDRA
-#define PIXEL_BIT  PORTA5
+#define PIXEL_PORT PORTB
+#define PIXEL_DDR  DDRB
+#define PIXEL_BIT  PORTB2
 /* PA5 is pin 8 */
 /* softuart uses PA1 for RX, PA2 for TX (pins 12 and 11) */
 
@@ -19,14 +17,6 @@
 
 static uint8_t grb[PIXELS*3];
 
-/* rgb input buffer */
-static uint8_t input[3];
-/* how many pixels we've read */
-static uint8_t pixels_read = 0;
-/* the byte we're reading in */
-static uint8_t byte = 0;
-/* which nibble we're reading */
-static uint8_t nibble = 0;
 
 void set_pixel(uint8_t i, uint8_t r, uint8_t g, uint8_t b)
 {
@@ -36,7 +26,8 @@ void set_pixel(uint8_t i, uint8_t r, uint8_t g, uint8_t b)
   grb[offset+2] = SCALE(b);
 }
 
-void write_pixels() {
+void write_pixels() 
+{
   cli(); /* disable interrupts */
   asm volatile(
       "1:"                    "\n\t" /* outer loop: iterate bytes */
@@ -64,81 +55,23 @@ void write_pixels() {
   sei(); /* enable interrupts */
 }
 
-void reset_input() {
-  nibble = byte = pixels_read = 0;
-  memset(&input, 0, 3);
-}
-
-void save_digit(char c)
-{
-  uint8_t value;
-  if (c >= '0' && c <= '9') {
-    value = c - '0';
-  }
-  else if (c >= 'a' && c <= 'f') {
-    value = 0xa + (c - 'a');
-  }
-  else if (c >= 'A' && c <= 'F') {
-    value = 0xa + (c - 'A');
-  }
-  else {
-    return;
-  }
-
-  /* save the value in the current input and nibble */
-  /* left to right, MSB first */
-  if (nibble == 0) {
-    value = value << 4;
-  }
-  input[byte] |= value;
-
-  /* then advance to the next nibble, byte, or pixel */
-  if (nibble == 0) {
-    nibble++;
-  }
-  else {
-    nibble = 0;
-    byte += 1;
-  }
-  if (byte == 3) {
-    set_pixel(pixels_read, input[0], input[1], input[2]);
-    pixels_read += 1;
-    byte = 0;
-    memset(&input, 0, 3);
-  }
-  if (pixels_read == PIXELS) {
-    reset_input();
-    write_pixels();
-    softuart_puts_P(".\r\n");
-  }
-}
-
 int main(void)
 {
-  char c;
+	/* set pixel pin to output */
+	PIXEL_DDR |= (1 << PIXEL_BIT);
+	sei(); /* enable interrupts */
+	//uint8_t pix = 0x03;
+	while (1){
+	
+		for (uint8_t pix = 0x00; pix < 0x12; pix++) {
+		set_pixel(pix, 0x00, 0x50, 0x00);
+		write_pixels();
+		_delay_ms(100);
 
-  /* set pixel pin to output */
-  PIXEL_DDR |= (1 << PIXEL_BIT);
-  softuart_init();
-  sei(); /* enable interrupts */
-  softuart_puts_P( "ready.\r\n" );
-  write_pixels();
-
-  for(;;) {
-    if (softuart_kbhit()) {
-      c = softuart_getchar();
-      if (c == 'x') {
-        reset_input();
-        softuart_puts_P("x\r\n");
-      }
-      else if (c == 'q') {
-        return 1;
-      }
-      else {
-        save_digit(c);
-      }
-    }
-  }
-
-  return 0;
+		set_pixel(pix, 0x00, 0x00, 0x00);
+		write_pixels();
+		_delay_ms(5);
+		}
+		
+	}
 }
